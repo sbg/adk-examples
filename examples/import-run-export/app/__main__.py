6e6b718d00556441ba30c9d884a3b9c2823efbaa
Christian Frech <christian.frech@sbgenomics.com>
2019-03-27 13:28:13 -0400

    add import-run-export

---
+++ examples/import-run-export/app/__main__.py
@@ -0,0 +1,116 @@
+import logging, json, datetime
+from collections import defaultdict
+from freyja import Automation, Step, Input, Output, List
+from freyja.types import Optional
+from hephaestus.steps import FindOrImportFiles, SetMetadataBulk, ExportFiles, \
+    FindOrCreateTask, RunTask
+from hephaestus.types import File
+from context import Context
+
+class Main(Step):
+    '''Example automation that imports FASTq files from volume, aligns 
+    them with BWA, and exports resulting BAM files back to volume. 
+    Inputs of the automation are name of the SB project, SB volume ID, 
+    and volume source and destination directories. 
+    Supports memoization, i.e. on re-run steps are only re-executed 
+    if relevant changes are detected (for example new FASTq files added).
+    
+    To run this automation on your computer, type the following command
+    while inside the project root directory:
+    
+      python -m app run --project_name <project-name> [--volume_id <volume-id> --src_dir <source-directory> --dest_dir <destination-directory>]
+    '''
+
+    project_name = Input(str, 
+        description="Name of SB execution project")   
+    volume_id = Input(str, 
+        description="ID of volume for file import and export",
+        default="external-demos/volumes_api_demo_bucket")
+    src_dir = Input(str, 
+        description="Source directory on volume containing input FASTq files",  
+        default="")
+    dest_dir = Input(str, 
+        description="Target directory on volume where outputs will be exported to",
+        default="automation/import-run-export/result")
+
+
+    def execute(self):
+        'step starts executing here'
+
+        # initialize automation context with execution project and volume
+        ctx = Context().initialize(self.project_name, self.volume_id)
+                
+        # import all fastq files found at volume source location
+        file_paths = [l.location for l in ctx.volume.list(prefix=str(self.src_dir))]
+        file_paths = [fp for fp in file_paths if fp.endswith(".fastq")]
+        
+        import_step = FindOrImportFiles("FindOrImportFiles",
+            filepaths=file_paths,
+            from_volume=ctx.volume,
+            to_project=ctx.project)
+
+        # set metadata for each imported file and group files by sample ID
+        samples = self.set_and_group_by_metadata(import_step.imported_files)
+        
+        # run BWA for each sample; samples are processed in parallel
+        bams = []
+        for sample_id, fastq_files in samples.items():
+            bwa_mem = BWAmem(f"BWAmem-{sample_id}", 
+                             input_reads=fastq_files)
+            bams.append(bwa_mem.aligned_reads)
+
+        # export bam files to volume
+        ExportFiles("ExportFiles",
+            files=bams,
+            to_volume=ctx.volume,
+            prefix=str(self.dest_dir),
+            overwrite=True)
+            
+            
+    def set_and_group_by_metadata(self, imported_files):
+        '''sets metadata for each imported file based on file name and
+        returns sample-to-file dict''' 
+        
+        metadata = []
+        samples = defaultdict(list)
+        
+        for file in imported_files:
+            
+            # example filename: TCRBOA7-N-WEX-TEST.read1.fastq
+            sample_id = file.name.split('-WEX')[0]
+            paired_end = file.name.split('read')[1].split('.')[0]
+            
+            metadata.append({'sample_id':sample_id, 'paired_end':paired_end})
+            samples[sample_id].append(file)
+  
+        # set metadata in bulk to minimize number of API calls          
+        SetMetadataBulk("SetMetadataBulk",
+            to_files=imported_files,
+            metadata=metadata)
+        
+        return samples
+
+class BWAmem(Step):
+    input_reads = Input(List[File])
+    aligned_reads = Output(File)
+        
+    def execute(self):
+        ctx = Context()
+        task = FindOrCreateTask(f"FindOrCreateTask-{self.name_}",
+            new_name = self.name_ + " - " + 
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            inputs={
+                'input_reads' : self.input_reads,
+                'reference_index_tar' : ctx.refs["bwa_bundle"]
+            },
+            app=ctx.apps["bwa"],
+            run=True,
+            in_project=ctx.project
+        ).task
+    
+        task = RunTask(f"RunTask-{self.name_}", task=task).finished_task
+        
+        self.aligned_reads = task.outputs['aligned_reads']
+        
+if __name__ == '__main__':
+    Automation(Main).run()
---
+++ examples/import-run-export/context.py
@@ -0,0 +1,64 @@
+import inject
+import os.path
+from freyja.config import Config
+from freyja.graph import Singleton
+from hephaestus.steps import FindOrCopyApp, FindOrCopyFilesByName, SBApi, FindOrCreateProject
+
+class Context(metaclass=Singleton):
+    '''Singleton class to store automation context information,
+    such as execution project, apps, and reference files.
+    
+    WARNING: Use context carefully in multi-threaded environments.
+    It should be initialized once at the beginning of execution
+    and then all access to it must be read-only. Otherwise
+    race conditions can cause nasty problems that are 
+    difficult to debug.'''
+
+    def __init__(self):
+        self.config = inject.instance(Config)
+        self.project = None
+        self.volume = None
+        self.apps = {}
+        self.refs = {}
+        
+    def initialize(self, project_name, volume_id):
+        'initializes context. read-only after this point.'''
+        
+        self.project = FindOrCreateProject(
+            "FindOrCreateProject",
+            billing_group_name=self.get_first_billing_group(), 
+            name=project_name
+        ).project
+        self.volume = SBApi().volumes.get(id=volume_id)
+
+        self.stage_apps()
+        self.stage_references() 
+        
+        return self
+
+    def get_first_billing_group(self):
+        for bg in SBApi().billing_groups.query().all():
+            return bg.name
+
+    def stage_apps(self):
+        for app_name, app_id in self.config.apps.data.items():
+            self.apps[app_name] = FindOrCopyApp(
+                f"FindOrCopyApp-{app_name}",
+                app_id=app_id,
+                to_project=self.project).app
+
+
+    def stage_references(self):
+        for ref_name, file_path in self.config.reference_files.data.items():
+            self.refs[ref_name] = self.stage_reference_file(ref_name, file_path)
+
+    def stage_reference_file(self, ref_name, file_path):
+        ref_project_id, file_name = os.path.split(file_path)
+        ref_project = SBApi().projects.get(id=ref_project_id) 
+        
+        return FindOrCopyFilesByName(f"CopyRef-{ref_name}",
+            names=[file_name], 
+            from_project=ref_project, 
+            to_project=self.project,
+        ).copied_files[0]
+                       
