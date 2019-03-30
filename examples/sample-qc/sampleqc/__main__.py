195306d8fe33ead9e2211246d32dba218ac5f000
Christian Frech <christian.frech@sbgenomics.com>
2019-03-30 14:12:29 -0400

    upload_qc_metrics()

--- examples/sample-qc/sampleqc/__main__.py
+++ examples/sample-qc/sampleqc/__main__.py
@@ -1,6 +1,6 @@
-import logging
+import logging, tempfile
 from freyja import Automation, Step, Input, List, Output, Optional
-from hephaestus.types import File
+from hephaestus import File, UploadFile
 from sampleqc.manifest import load_manifest
 from sampleqc.context import Context
 from sampleqc.apps import BWAmem, Trimgalore, \
@@ -17,8 +17,10 @@ class Main(Step):
     
     manifest_filename = Input(str)
     project_name = Input(str)
+    qc_summary = Output(File)
 
     def execute(self):
+        'Main execution method. Execution starts here.'
         
         # setup execution project, stage apps, ref files
         Context().initialize(project_name=self.project_name)
@@ -46,13 +48,50 @@ class Main(Step):
             )
 
             # process BAM conditioned on alignment QC
-            processbam = ProcessBam(f"ProcessBam-{sample.id}",
+            sample.processbam = ProcessBam(f"ProcessBam-{sample.id}",
                 input_bam = bwa.merged_bam,
                 sample_id = sample.id
             )
             
-            sample.bam = processbam.processed_bam
+        # upload QC metrics summary and return on output
+        self.qc_summary = self.upload_qc_metrics(cohort)
+        
 
+    def upload_qc_metrics(self, cohort):
+        '''Collects BAM QC metrics from all processed samples and uploads
+        summary file in tab-separated format to SB project. Overwrites 
+        existing file. Returns uploaded file object.'''
+        
+        # note: don't create a transient temporary file (not thread safe) 
+        temp_filename = tempfile.gettempdir() + "/bam_qc_metrics.tsv"
+        temp = open(temp_filename, "wt")
+            
+        # write header
+        temp.write("\t".join([
+            "sample_id", 
+            "bam_file", 
+            "pct_pf_reads_aligned", 
+            "strand_balance",
+            "status"]) + "\n")
+        
+        # write content
+        for sample in cohort.samples:
+            bam_step = sample.processbam
+            temp.write("\t".join([
+                sample.id,
+                bam_step.input_bam.name,
+                str(bam_step.qc_metrics['pct_pf_reads_aligned']),
+                str(bam_step.qc_metrics['strand_balance']),
+                bam_step.qc_metrics['status']]) + "\n") 
+        temp.close()
+        
+        # upload file to platform (overwrites existing file)
+        return UploadFile("UploadMetrics",
+            local_path=temp.name, 
+            to_project=Context().project, 
+            overwrite=True).file
+
+        
 class FilterFastq(Step):
     'Filters out FASTq files not meeting QC criteria'
     
@@ -73,15 +112,16 @@ class ProcessBam(Step):
     input_bam = Input(File)
     sample_id = Input(str)
     processed_bam = Output(Optional[File])
+    qc_metrics = Output(dict)
 
     def execute(self):
 
         # compute alignment quality metrics
         alignment_qc = PicardAlignmentSummaryMetrics(
             f"AlignmentSummaryMetrics-{self.sample_id}",
-            input_bam=self.input_bam, 
+            input_bam=self.input_bam 
         )
-
+        
         # if BAM failed QC, we are done here (dynamic conditional)
         if alignment_qc.metrics_ok():
 
@@ -96,6 +136,13 @@ class ProcessBam(Step):
         else:
             logging.info(f"Sample failed QC: {self.input_bam.name}")
             self.processed_bam = None
+
+        self.qc_metrics = { 
+            'pct_pf_reads_aligned' : float(alignment_qc.pct_pf_reads_aligned),
+            'strand_balance' : float(alignment_qc.strand_balance),
+            'status' : "PASS" if alignment_qc.metrics_ok() else "FAIL"
+        }
+
                 
 
 if __name__ == '__main__':
