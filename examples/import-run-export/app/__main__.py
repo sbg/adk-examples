7ede2d0e2d0c706c9975a98c94972873feeee20c
Christian Frech <christian.frech@sbgenomics.com>
2019-04-10 21:34:37 -0400

    reformatting

--- examples/import-run-export/app/__main__.py
+++ examples/import-run-export/app/__main__.py
@@ -5,10 +5,11 @@ from hephaestus import (
     SetMetadataBulk,
     ExportFiles,
     FindOrCreateAndRunTask,
-    File
+    File,
 )
 from app.context import Context
 from app.types import Sample
+
 
 class Main(Step):
     """Example automation that imports FASTq files from volume, aligns 
@@ -47,16 +48,13 @@ class Main(Step):
 
         # stage input FASTq files and group them into samples
         samples = ImportFiles(src_dir=self.src_dir).samples
-        
+
         # run BWA for each sample; samples are processed in parallel
         # because app outputs are promises and we can access them
         # before output values become available (lazy evaluation)
         bams = []
         for sample in samples:
-            bwa = BWAmem(
-                f"BWAmem-{sample.sample_id}", 
-                input_reads=sample.fastq_files
-            )
+            bwa = BWAmem(f"BWAmem-{sample.sample_id}", input_reads=sample.fastq_files)
             bams.append(bwa.aligned_reads)
 
         # export all BAM files to volume; export step starts executing
@@ -65,23 +63,24 @@ class Main(Step):
             files=bams, to_volume=ctx.volume, prefix=self.dest_dir, overwrite=True
         )
 
+
 class ImportFiles(Step):
-    '''Finds FASTq files on volume, imports them into project, sets file 
-    metadata, and returns updated files grouped into list of samples'''
-    
+    """Finds FASTq files on volume, imports them into project, sets file 
+    metadata, and returns updated files grouped into list of samples"""
+
     src_dir = Input(str)
     samples = Output(List[Sample])
-    
+
     def execute(self):
         imported_files = self.import_files_from_volume()
         updated_files = self.update_file_metadata(imported_files)
         self.samples = self.group_files_into_samples(updated_files)
-        
+
     def import_files_from_volume(self):
-        'Imports all fastq files found at volume source location'
-        
+        "Imports all fastq files found at volume source location"
+
         volume = Context().volume
-        
+
         fastq_paths = [
             l.location
             for l in volume.list(prefix=self.src_dir)
@@ -89,15 +88,13 @@ class ImportFiles(Step):
         ]
 
         return FindOrImportFiles(
-            filepaths=fastq_paths, 
-            from_volume=volume, 
-            to_project=Context().project
+            filepaths=fastq_paths, from_volume=volume, to_project=Context().project
         ).imported_files
-        
+
     def update_file_metadata(self, files):
-        '''Sets file metadata in bulk for list of files based on file names.
+        """Sets file metadata in bulk for list of files based on file names.
         Setting metadata in bulk instead of per-file reduces API calls.
-        Example filename: TCRBOA7-N-WEX-TEST.read1.fastq'''
+        Example filename: TCRBOA7-N-WEX-TEST.read1.fastq"""
 
         metadata = []
         for file in files:
@@ -105,15 +102,12 @@ class ImportFiles(Step):
             paired_end = file.name.split("read")[1].split(".")[0]
             metadata.append({"sample_id": sample_id, "paired_end": paired_end})
 
-        return SetMetadataBulk(
-            to_files=files, metadata=metadata
-        ).updated_files
+        return SetMetadataBulk(to_files=files, metadata=metadata).updated_files
 
-        
     def group_files_into_samples(self, files):
-        '''Groups files into list of sample objects for easier downstream
-        processing.'''
-        
+        """Groups files into list of sample objects for easier downstream
+        processing."""
+
         samples = {}
         for file in files:
             sample_id = file.metadata["sample_id"]
@@ -122,7 +116,7 @@ class ImportFiles(Step):
             samples[sample_id].fastq_files.append(file)
 
         return list(samples.values())
-    
+
 
 class BWAmem(Step):
     """App wrapper step that runs BWA-MEM on SB platform. 
@@ -134,7 +128,7 @@ class BWAmem(Step):
     def execute(self):
         ctx = Context()
         task = FindOrCreateAndRunTask(
-            new_name="BWAmem - " 
+            new_name="BWAmem - "
                 + self.input_reads[0].metadata["sample_id"] + " - "
                 + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             inputs={
--- examples/import-run-export/app/types.py
+++ examples/import-run-export/app/types.py
@@ -1,24 +1,24 @@
 from freyja import Type, List
 from hephaestus import File
 
+
 class Sample(Type):
-    '''Custom type representing sample with input files. Use as an example 
+    """Custom type representing sample with input files. Use as an example 
     to see how to create custom types that can be used as step inputs and 
-    outputs. Custom types must implement a serialize and deserialize method.'''
-    
+    outputs. Custom types must implement a serialize and deserialize method."""
+
     def __init__(self, sample_id):
         self.sample_id = sample_id
         self.fastq_files = []
-    
+
     @classmethod
     def _serialize(cls, val):
         return {
-            'sample_id': val.sample_id,
-            'fastq_files': List[File]._serialize(val.fastq_files)
+            "sample_id": val.sample_id,
+            "fastq_files": List[File]._serialize(val.fastq_files),
         }
 
     @classmethod
     def _deserialize(cls, val):
-        val.sample_id = val['sample_id']
-        val.fastq_files = List[File]._deserialize(val['fastq_files'])
-        
+        val.sample_id = val["sample_id"]
+        val.fastq_files = List[File]._deserialize(val["fastq_files"])
